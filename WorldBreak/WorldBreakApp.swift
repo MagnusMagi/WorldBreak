5de4f877a9442249694f47cc3f5c8f9ca86f011c
MagnusMagi <magnusmagi.temp@gmail.com>
2025-09-16 21:32:57 +0300

    Initial Commit

---
+++ WorldBreak/WorldBreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldBreakApp.swift
+//  WorldBreak
+//
+//  Created by Magnus Magi on 16.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct WorldBreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
